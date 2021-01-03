4640d219345e054fcfb615c2e26d2ced66b084e8
coffee-dan <coffee-dan@protonmail.com>
2021-01-02 22:16:57 -0600

    Simple form with template writeup

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,9 @@
 "use strict";
+// const container: HTMLElement | any = document.getElementById('app');
+// const testElement: string = `
+//     <div class="test">
+//         <h1>name here</h1>
+//     </div>
+// `;
+// container.innerHTML += testElement;
 console.log('Hello World.ts');
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+// const container: HTMLElement | any = document.getElementById('app');
+
+// const testElement: string = `
+//     <div class="test">
+//         <h1>name here</h1>
+//     </div>
+// `;
+
+// container.innerHTML += testElement;
 console.log('Hello World.ts');

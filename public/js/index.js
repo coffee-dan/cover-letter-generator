282da68c097c1f70387ffafeb141ef8fb20b485f
coffee-dan <coffee-dan@protonmail.com>
2021-01-07 19:19:27 -0600

    Up to point of generation
    Simplified initial form
    Error checking
    Boundary cases handled
    Continuing with design constaint of a one page, no reloading experience

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,89 @@
 "use strict";
-// const container: HTMLElement | any = document.getElementById('app');
-// const testElement: string = `
-//     <div class="test">
-//         <h1>name here</h1>
-//     </div>
-// `;
-// container.innerHTML += testElement;
-console.log('Hello World.ts');
+var coverLetter = document.getElementById('cover_letter');
+var nameElement = document.getElementById('name');
+var info = document.getElementById('info');
+var letterBody = document.getElementById('p1');
+var signature = document.getElementById('sign');
+var substitutionsForm = document.getElementById('substitutions');
+var placeholdersList;
+var substitutionsDict;
+function isFormFilled() {
+    return (nameElement.value != '' &&
+        info.value != '' &&
+        letterBody.value != '' &&
+        signature.value != '');
+}
+// COVER LETTER SUBMIT
+coverLetter.addEventListener('submit', function (event) {
+    // prevent the default behavior of the submit button
+    event.preventDefault();
+    console.log('Cover Letter Text Submitted!');
+    console.log('isFormFilled()', isFormFilled());
+    if (!isFormFilled()) {
+        return;
+    }
+    var text = nameElement.value + info.value + letterBody.value + signature.value;
+    console.log('text', text);
+    placeholdersList = findPlaceholders(text);
+    console.log('findPlaceholders(text)', placeholdersList);
+    generateSubstitutionsForm(placeholdersList);
+});
+// SUBSTITUTIONS SUBMIT
+substitutionsForm.addEventListener('submit', function (event) {
+    // prevent the default behavior of the submit button
+    event.preventDefault();
+    console.log('Substitutions form submitted!');
+    console.log('isFormFilled()', isFormFilled());
+    if (!isFormFilled()) {
+        return;
+    }
+    var text = nameElement.value + info.value + letterBody.value + signature.value;
+    console.log('text', text);
+    placeholdersList = findPlaceholders(text);
+    console.log('findPlaceholders(text)', placeholdersList);
+    generateSubstitutionsForm(placeholdersList);
+});
+function generateSubstitutionsForm(placeholders) {
+    if (placeholders.length <= 0) {
+        substitutionsForm.innerHTML = "<div>No substitutions</div>";
+        return false;
+    }
+    substitutionsForm.innerHTML = '';
+    var i;
+    for (i = 0; i < placeholders.length; i++) {
+        substitutionsForm.innerHTML += "<label for=\"name\">" + placeholders[i] + ":</label><br />\n\t\t<input class=\"form_element\" type=\"text\" id=\"placeholder" + i + "\" name=\"placeholder" + i + "\" placeholder=\"Text to replace " + placeholders[i] + "\" />\n\t\t<br />";
+    }
+    substitutionsForm.innerHTML += "<input type=\"submit\" value=\"Generate Cover Letter\">";
+    return true;
+}
+// Find placeholders of format [placeholder]
+function findPlaceholders(text) {
+    var phRegex = new RegExp(/\[[^[]*\]/g);
+    var placeholders = text.match(phRegex) || [];
+    return placeholders;
+}
+// Get replacements for each of the placeholders
+function getReplacements(placeholders) {
+    var dict = {};
+    var i;
+    for (i = 0; i < placeholders.length; i++) {
+        dict[placeholders[i]] = i.toString();
+    }
+    return dict;
+}
+function fillPlaceholders(text, dict) {
+    var newText = text;
+    Object.keys(dict).map(function (placeholder) {
+        newText = newText.replaceAll(placeholder, dict[placeholder]);
+    });
+    return newText;
+}
+// const text: string =
+// 	"Hello, my name is [name] i'm applying for [job_position] at [company_name]. [job_position] is cool";
+// console.log('wowo');
+// const phList: string[] = findPlaceholders(text);
+// console.log('findPlaceholders(text)', phList);
+// const dict: placeholderDictionary = getReplacements(phList);
+// console.log('getReplacements(phList)', dict);
+// const newText: string = fillPlaceholders(text, dict);
+// console.log('fillPlaceholders(text, dict)', newText);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,129 @@
-// const container: HTMLElement | any = document.getElementById('app');
+const coverLetter: HTMLElement | any = document.getElementById('cover_letter');
+const nameElement: HTMLElement | any = document.getElementById('name');
+const info: HTMLElement | any = document.getElementById('info');
+const letterBody: HTMLElement | any = document.getElementById('p1');
+const signature: HTMLElement | any = document.getElementById('sign');
+let substitutionsForm: HTMLElement | any = document.getElementById(
+	'substitutions'
+);
 
-// const testElement: string = `
-//     <div class="test">
-//         <h1>name here</h1>
-//     </div>
-// `;
+interface placeholderDictionary {
+	[placeholder: string]: string;
+}
 
-// container.innerHTML += testElement;
-console.log('Hello World.ts');
+let placeholdersList: string[];
+let substitutionsDict: placeholderDictionary;
+
+function isFormFilled(): boolean {
+	return (
+		nameElement.value != '' &&
+		info.value != '' &&
+		letterBody.value != '' &&
+		signature.value != ''
+	);
+}
+
+// COVER LETTER SUBMIT
+coverLetter.addEventListener('submit', (event: Event) => {
+	// prevent the default behavior of the submit button
+	event.preventDefault();
+
+	console.log('Cover Letter Text Submitted!');
+	console.log('isFormFilled()', isFormFilled());
+
+	if (!isFormFilled()) {
+		return;
+	}
+
+	const text =
+		nameElement.value + info.value + letterBody.value + signature.value;
+
+	console.log('text', text);
+
+	placeholdersList = findPlaceholders(text);
+	console.log('findPlaceholders(text)', placeholdersList);
+	generateSubstitutionsForm(placeholdersList);
+});
+
+// SUBSTITUTIONS SUBMIT
+substitutionsForm.addEventListener('submit', (event: Event) => {
+	// prevent the default behavior of the submit button
+	event.preventDefault();
+
+	console.log('Substitutions form submitted!');
+	console.log('isFormFilled()', isFormFilled());
+
+	if (!isFormFilled()) {
+		return;
+	}
+
+	const text =
+		nameElement.value + info.value + letterBody.value + signature.value;
+
+	console.log('text', text);
+
+	placeholdersList = findPlaceholders(text);
+	console.log('findPlaceholders(text)', placeholdersList);
+	generateSubstitutionsForm(placeholdersList);
+});
+
+function generateSubstitutionsForm(placeholders: string[]): boolean {
+	if (placeholders.length <= 0) {
+		substitutionsForm.innerHTML = `<div>No substitutions</div>`;
+		return false;
+	}
+
+	substitutionsForm.innerHTML = '';
+
+	let i: number;
+	for (i = 0; i < placeholders.length; i++) {
+		substitutionsForm.innerHTML += `<label for="name">${placeholders[i]}:</label><br />
+		<input class="form_element" type="text" id="placeholder${i}" name="placeholder${i}" placeholder="Text to replace ${placeholders[i]}" />
+		<br />`;
+	}
+
+	substitutionsForm.innerHTML += `<input type="submit" value="Generate Cover Letter">`;
+
+	return true;
+}
+
+// Find placeholders of format [placeholder]
+function findPlaceholders(text: string): string[] {
+	const phRegex: RegExp = new RegExp(/\[[^[]*\]/g);
+	const placeholders: string[] = text.match(phRegex) || [];
+	return placeholders;
+}
+
+// Get replacements for each of the placeholders
+function getReplacements(placeholders: string[]): placeholderDictionary {
+	let dict: placeholderDictionary = {};
+	let i;
+	for (i = 0; i < placeholders.length; i++) {
+		dict[placeholders[i]] = i.toString();
+	}
+
+	return dict;
+}
+
+function fillPlaceholders(text: string, dict: placeholderDictionary): string {
+	let newText: string = text;
+
+	Object.keys(dict).map(placeholder => {
+		newText = newText.replaceAll(placeholder, dict[placeholder]);
+	});
+
+	return newText;
+}
+
+// const text: string =
+// 	"Hello, my name is [name] i'm applying for [job_position] at [company_name]. [job_position] is cool";
+
+// console.log('wowo');
+// const phList: string[] = findPlaceholders(text);
+// console.log('findPlaceholders(text)', phList);
+
+// const dict: placeholderDictionary = getReplacements(phList);
+// console.log('getReplacements(phList)', dict);
+
+// const newText: string = fillPlaceholders(text, dict);
+// console.log('fillPlaceholders(text, dict)', newText);
